a8bfc80808d8e97e307afd26068e1a97e3a5166f
gingerswaltz <115674180+gingerswaltz@users.noreply.github.com>
2023-04-29 14:51:40 +0900

    +++

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,3 +1,63 @@
+//// the setup function runs once when you press reset or power the board
+//const int POTENTIOMETER_PIN = PA7;
+//const int SEGMENT_A_PIN = PB9;
+//const int SEGMENT_B_PIN = PB5;
+//const int SEGMENT_C_PIN = PB4;
+//const int SEGMENT_D_PIN = PB11;
+//const int SEGMENT_E_PIN = PB10;
+//const int SEGMENT_F_PIN = PB8;
+//const int SEGMENT_G_PIN = PB3;
+//
+//
+////todo: dot pin , second display pins??
+//int segPins[] = {PB9, PB5, PB4, PB11, PB10, PB8, PB3};   // Пины сегментов первого индикатора (в порядке a,b,c,d,e,f,g,  ??dp)
+//int digit1, digit2;  // Переменные для хранения первой и второй цифр
+//
+//
+////
+////
+////
+//void setup() {
+//  Serial.begin(9600);
+//  pinMode(PB13, OUTPUT);
+//  pinMode(PB15, OUTPUT);
+//  pinMode(POTENTIOMETER_PIN, INPUT);
+//  pinMode(SEGMENT_G_PIN, OUTPUT); // g
+//  pinMode(SEGMENT_C_PIN, OUTPUT); // c
+//  pinMode(SEGMENT_B_PIN, OUTPUT); // b
+//  pinMode(SEGMENT_D_PIN, OUTPUT); // d
+//  pinMode(SEGMENT_E_PIN, OUTPUT); // e
+//  pinMode(SEGMENT_F_PIN, OUTPUT); // f
+//  pinMode(SEGMENT_A_PIN, OUTPUT); // a
+//  pinMode(PB1, OUTPUT);
+//  pinMode(PB0, OUTPUT);
+//
+//}
+//
+//
+//int digits[10][8] = {            // Массив, хранящий информацию о том, какие сегменты должны быть включены для каждой цифры
+//  {1,1,1,1,1,1,0,0},            // 0
+//  {0,1,1,0,0,0,0,0},            // 1
+//  {1,1,0,1,1,0,1,0},            // 2
+//  {1,1,1,1,0,0,1,0},            // 3
+//  {0,1,1,0,0,1,1,0},            // 4
+//  {1,0,1,1,0,1,1,0},            // 5
+//  {1,0,1,1,1,1,1,0},            // 6
+//  {1,1,1,0,0,0,0,0},            // 7
+//  {1,1,1,1,1,1,1,0},            // 8
+//  {1,1,1,1,0,1,1,0}             // 9
+//};
+//
+//
+//
+//// the loop function runs over and over again forever
+//void loop() {
+//
+//int angle =map(analogRead(POTENTIOMETER_PIN), 1, 4095, 0, 20); // Считываем значение с потенциометра
+//
+//
+//}
+
 // the setup function runs once when you press reset or power the board
 const int POTENTIOMETER_PIN = PA7;
 const int SEGMENT_A_PIN = PB9;
@@ -7,17 +67,12 @@ const int SEGMENT_D_PIN = PB11;
 const int SEGMENT_E_PIN = PB10;
 const int SEGMENT_F_PIN = PB8;
 const int SEGMENT_G_PIN = PB3;
-
-
 //todo: dot pin , second display pins??
 int segPins[] = {PB9, PB5, PB4, PB11, PB10, PB8, PB3};   // Пины сегментов первого индикатора (в порядке a,b,c,d,e,f,g,  ??dp)
 int segPins2[] = {}; // Пины сегментов второго индикатора
 int digit1, digit2;              // Переменные для хранения первой и второй цифр
+int d2, d1;
 
-
-
-//
-//
 //
 void setup() {
   Serial.begin(9600);
@@ -37,41 +92,69 @@ void setup() {
 }
 
 
-byte digits[] = { 
- 0b11000000, // 0 
- 0b11111001, // 1 
- 0b10100100, // 2 
- 0b10110000, // 3 
- 0b10011001, // 4 
- 0b10010010, // 5 
- 0b10000010, // 6 
- 0b11111000, // 7 
- 0b10000000, // 8 
- 0b10010000 // 9 
+int digits[10][7] = {            // Массив, хранящий информацию о том, какие сегменты должны быть включены для каждой цифры
+  {1,1,1,1,1,1,0},            // 0
+  {0,1,1,0,0,0,0},            // 1
+  {1,0,1,1,1,0,1},            // 2
+  {1,1,1,1,0,0,1},            // 3
+  {0,1,1,0,0,1,1},            // 4
+  {1,1,0,1,0,1,1},            // 5
+  {1,1,0,1,1,1,1},            // 6
+  {1,1,1,0,0,0,0},            // 7
+  {1,1,1,1,1,1,1},            // 8
+  {1,1,1,1,0,1,1}             // 9
 };
 
-
-void displayDigit(byte digit, int digitPin, int segPins[]){
-  for (int i=0; i<7; i++){
-    if (digit &(1<<i)){
-      digitalWrite(segPins[i], HIGH);
-    }else {
-      digitalWrite(segPins[i], LOW);
-    }
-  }
-
-  digitalWrite(digitPin, LOW);
-  delayMicroseconds(500);
-  digitalWrite(digitPin, HIGH);
-}
 
 // the loop function runs over and over again forever
 void loop() {
 
-int angle =map(analogRead(POTENTIOMETER_PIN), 1, 4095, 0, 20); // Считываем значение с потенциометра
- Serial.println(angle);
-  Serial.println(angle%10);
-  ////digit1 =     // Преобразуем первую цифру в диапазон от 0 до 9
-//  digit2 = digit1 + 1;                    // Вычисляем вторую цифру как следующую после первой
- displayDigit(digits[4], PB14, segPins); 
+int potValue = analogRead(POTENTIOMETER_PIN);    // Считываем значение с потенциометра
+  digitalWrite(PB13,HIGH);
+  digitalWrite(PB15, LOW);
+  digit1 = map(potValue, 0, 4095, 0, 20);    // Преобразуем первую цифру в диапазон от 0 до 9
+  digit2 = digit1 + 1;                    // Вычисляем вторую цифру как следующую после первой
+  
+  // Ограничиваем вторую цифру диапазоном от 0 до 9
+  
+  if (digit1>9)
+  {     
+    for (int i=0; i<9; i++)
+    {
+      digitalWrite(PB13, LOW);
+      digitalWrite(PB15, LOW);
+      digitalWrite(PB15, HIGH);
+      digitalWrite(segPins[i], digits[1][i]);
+      delay(1000);
+      digitalWrite(PB13, LOW);
+      digitalWrite(PB15, LOW);
+      digitalWrite(PB13, HIGH); 
+      digitalWrite(segPins[i], digits[i][i]);
+      delay(1000);
+    }
+
+ 
+  }
+  else
+    {
+      digitalWrite(PB15, LOW);
+      for (int i = 0; i < 10; i++) 
+        {
+          digitalWrite(PB13, HIGH);
+          digitalWrite(segPins[i], digits[i][i]);
+          delay(1);
+
+        }
+
+
+    }
+  
+  // Отображаем первую цифру на левом индикаторе
+  
+
+
+  
+  // Отображаем вторую цифру на правом индикаторе
+  
+ 
 }
--- laba2/laba2.ino
+++ laba2/laba2.ino
@@ -1,5 +1,6 @@
 #include "LiquidCrystal_I2C.h" // библиотека для экранчика..
-
+#include <Wire.h>
+const double pi=3.14;
 LiquidCrystal_I2C lcd( 0x27,20, 4); // инициализация объекта lcd
 char buffer [6];
 byte digitsArr[10] = {
@@ -15,14 +16,26 @@ byte digitsArr[10] = {
   0b01101111, //9
 };
 
+int r_mas[4]={50, 75, 100, 150};
+int XL_mas[5]={200, 250, 300, 350, 400};
+int r, XL;
+ int u=50;
+ double X;
 
 int segPins[14]={0, 0, PB0, PB1, PB3, PB4, PB5, PB8, PB10, PB11, 0, 0, PB12, PB13}; //pb12, pb13 resistors
 int pause = 1;
-
+int xc=0;
 void setup() 
 {
-  lcd.init();                       // запускаем экран 
-  lcd.backlight();                  // Запускаем подсветку экрана
+  randomSeed(analogRead(PB9));      // задаем зерно рандомной генерации  
+ r=r_mas[random(0,4)];
+ XL=XL_mas[random(0,5)];
+lcd.print("test");
+ lcd.init();                       // запускаем экран 
+ lcd.backlight();                  // Запускаем подсветку экрана
+ Serial.begin(115200);
+pinMode(PA5, INPUT);
+pinMode(PA3, INPUT);
   for (int i=2;i<=13;i++){
       pinMode(segPins[i], OUTPUT);
   }
@@ -58,33 +71,41 @@ void PotCheck(int digit){
 
 }
 
-void TumblerCheck(int digit){
-int tumbler_state=digitalRead(PA3); // поменять пин?
-  if (tumbler_state>0)
-  {
-      int capacity=(digit)*145;                
-    lcd.setCursor(0,1);
-    lcd.print("C=");
-    lcd.setCursor(1,1);
-    lcd.print(":");
-    lcd.setCursor(2,1);
-    dtostrf(capacity, 4, 1, buffer);             // Формат до 1 знака после запятой
-    lcd.print(buffer);
-    delay(10);
-    lcd.setCursor(8,1);
-    lcd.print("mkF");
-  }
-}
+
 
 void loop() 
 {
+
+int  digit=map(analogRead(PA5), 20, 4095, 1, 21);
+digit=constrain(digit, 1, 20);
+Serial.println(digitalRead(PA3));
+    PotCheck(digit);
+
+  
+int tumbler_state=digitalRead(PA3); 
+  if (tumbler_state>0)
+  {
+
+xc=0;
+     
+  } else {
     
-  int digit=map(analogRead(PA0), 0, 1023, 1, 20);
-
-  PotCheck(digit);
-  
-TumblerCheck(digit);
-
-  
+  xc=(1000000/2*pi*50*digit);
+  }
+    
+    X=XL-xc;
+   
+   double Z=sqrt(r*r+_X*_X);
+   double I=(u/Z);
+   lcd.setCursor(0,0);
+    lcd.print("A1");
+    lcd.setCursor(1,0);
+    lcd.print(":");
+    lcd.setCursor(2,0);
+    dtostrf(I, 4, 1, buffer);             // Формат до 1 знака после запятой
+    
+    lcd.print(buffer);
+    lcd.setCursor(7,0);
+    lcd.print("v");
  
 }
